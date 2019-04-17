43c01fc3ba7ce7918ec69b6d389cc39799b815c1
Steven Huang <steven.charles.huang@gmail.com>
2019-04-17 02:05:43 -0400

    code cleanup, comments, style cleanup

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,10 @@ $(document).ready(() => {
   let curPlayer = 'x';
   let isGameOver = false;
 
+  /**
+   * @function createBoard
+   * @description Creates the Tic Tac Toe Board
+   */
   const createBoard = () => {
     boardArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     $gameArea.append(`<div class="game-board" />`);
@@ -23,8 +27,12 @@ $(document).ready(() => {
     }
   };
 
+  /**
+   * @function checkWin
+   * @description Checks if the current move wins the game for the current player
+   * @param {string} player 
+   */
   const checkWin = (player) => {
-    console.log(boardArr);
     if (
       (boardArr[0] === boardArr[1] && boardArr[1] === boardArr[2]) ||
       (boardArr[3] === boardArr[4] && boardArr[4] === boardArr[5]) ||
@@ -46,13 +54,18 @@ $(document).ready(() => {
     }
   };
 
+  /**
+   * @function handleTileClick
+   * @description Event Handler for tile clicks
+   * @param {object} e 
+   */
   const handleTileClick = (e) => {
     if (!isGameOver) {
       let tileNum = e.target.dataset.id;
-
+      // Conditional check for whether a tile has been played already
       if (typeof boardArr[tileNum] === 'number' ) {
         $(`#tile-${tileNum}`).append(`<div class="player-mark noselect">${curPlayer}</div>`);
-        
+        // Updates tile and calls checkWin() based on current player
         if (curPlayer === 'x') {
           boardArr[tileNum] = 'x';
           checkWin(curPlayer);
@@ -60,31 +73,45 @@ $(document).ready(() => {
           boardArr[tileNum] = 'o';
           checkWin(curPlayer);
         }
-        
       } else {
         updateMsg('That tile\`s occupied, bub', true);
       }
     }
   };
 
+  /**
+   * @function updateMsg
+   * @description Updates the message box temproarily or semi-permanently given an input message
+   * @param {string} msg 
+   * @param {Boolean} isTemporary 
+   */
   const updateMsg = (msg, isTemporary = false) => {
     if (!isTemporary) {
+      // Semi-permanent message update
       $($messageBox).text(msg);
     } else {
+      // Temporary message update
       let prevText = $($messageBox).text();
 
       $($messageBox).text(msg);
+      // Restore message text to previous message after 1.5 sec
       setTimeout(() => $($messageBox).text(prevText), 1500);
     }
   }
 
+  /**
+   * @function initBoard
+   * @description Initializes the Tic Tac Toe board
+   */
   const initBoard = () => { 
     if ($gameArea.children().length > 0) {
       $gameBoard.remove();
       $playAgain.remove();
     }
+    // Init the boardarr, create the board, init the player at turn, init isGameOver var, adds event handler to tiles
     boardArr = [];
     createBoard();
+    curPlayer = 'x';
     updateMsg('Player X\'s move');
     isGameOver = false;
     $tile = $('.tile');
@@ -92,9 +119,5 @@ $(document).ready(() => {
   }
 
   initBoard();
-
-
-
-
-
 });
+
