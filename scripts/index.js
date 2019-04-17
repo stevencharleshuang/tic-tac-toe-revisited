b933b16943eb2734d699683a6ea8884255be5ca1
Steven Huang <steven.charles.huang@gmail.com>
2019-04-17 01:48:32 -0400

    completed basic game

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,23 @@ $(document).ready(() => {
   const $document = $(document);
   const $body = $(document.body);
   const $gameArea = $('.game-area');
-  const $messageBox = $('.messageBox');
+  const $messageBox = $('.message-box');
+  let $gameBoard;
+  let $playAgain;
   let $tile;
 
   // JS Var Decs
-  let boardArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  let boardArr;
   let curPlayer = 'x';
+  let isGameOver = false;
 
   const createBoard = () => {
+    boardArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    $gameArea.append(`<div class="game-board" />`);
+    $gameBoard = $('.game-board');
     for (let i = 0; i < 9; i += 1) {
       $tile = `<div class="tile" id="tile-${i}" data-id="${i}" />`;
-      $($gameArea).append($tile);      
+      $($gameBoard).append($tile);      
     }
   };
 
@@ -29,33 +35,66 @@ $(document).ready(() => {
       (boardArr[0] === boardArr[4] && boardArr[4] === boardArr[8]) ||
       (boardArr[2] === boardArr[4] && boardArr[4] === boardArr[6])
       ) {
-      console.log(`player ${player} wins`);
+      updateMsg(`Player ${player.toUpperCase()} Wins!`);
+      isGameOver = true;
+      $body.append(`<div class="play-again noselect">Play Again?</div>`);
+      $playAgain = $('.play-again');
+      $($playAgain).on('click', () => initBoard());
+    } else {
+      player === 'x' ? curPlayer = 'o' : curPlayer = 'x';
+      updateMsg(`Player ${curPlayer.toUpperCase()}'s move`);
     }
   };
 
   const handleTileClick = (e) => {
-    let tileNum = e.target.dataset.id;
-    console.log('handling tile click', e.target.dataset.id);
-    if (typeof boardArr[tileNum] === 'number' ) {
-      $(`#tile-${tileNum}`).append(`<div class="player-mark">${curPlayer}</div>`);
+    if (!isGameOver) {
+      let tileNum = e.target.dataset.id;
 
-      if (curPlayer === 'x') {
-        boardArr[tileNum] = 'x';
-        checkWin(curPlayer);
-        curPlayer = 'o';
+      if (typeof boardArr[tileNum] === 'number' ) {
+        $(`#tile-${tileNum}`).append(`<div class="player-mark noselect">${curPlayer}</div>`);
+        
+        if (curPlayer === 'x') {
+          boardArr[tileNum] = 'x';
+          checkWin(curPlayer);
+        } else {
+          boardArr[tileNum] = 'o';
+          checkWin(curPlayer);
+        }
+        
       } else {
-        boardArr[tileNum] = 'o';
-        checkWin(curPlayer);
-        curPlayer = 'x';
+        updateMsg('That tile\`s occupied, bub', true);
       }
-    } else {
-      console.log('tile is occupied');
     }
   };
 
-  // Init Board
-  createBoard();
+  const updateMsg = (msg, isTemporary = false) => {
+    if (!isTemporary) {
+      $($messageBox).text(msg);
+    } else {
+      let prevText = $($messageBox).text();
 
-  $tile = $('.tile');
-  $($tile).on('click', (e) => handleTileClick(e));
+      $($messageBox).text(msg);
+      setTimeout(() => $($messageBox).text(prevText), 1500);
+    }
+  }
+
+  const initBoard = () => { 
+    if ($gameArea.children().length > 0) {
+      $gameBoard.remove();
+      $playAgain.remove();
+    }
+    boardArr = [];
+    createBoard();
+    updateMsg('Player X\'s move');
+    isGameOver = false;
+    $tile = $('.tile');
+    $($tile).on('click', (e) => handleTileClick(e));
+  }
+
+  initBoard();
+
+
+
+
+
 });
